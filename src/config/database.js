19a4572a7caaaa905de20de3b3211c3d190bc230
Tegar Pamungkas <mtegarpamungkas111@gmail.com>
2024-08-22 16:04:20 +0700

    update

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI_LOCAL) {
+    if (!process.env.MONGO_URI) {
       throw new Error("MONGO_URI is not defined in .env file");
     }
     await mongoose.connect(process.env.MONGO_URI, {});

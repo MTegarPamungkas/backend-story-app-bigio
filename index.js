0ca5fecd7534642cc809699af888f74885a45477
Tegar Pamungkas <mtegarpamungkas111@gmail.com>
2024-08-22 13:14:22 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const storyRoutes = require("./src/frameworks/routes/storyRoutes");
-const connectDB = require("./src/config/database");
+const storyRoutes = require("./frameworks/routes/storyRoutes");
+const connectDB = require("./config/database");
 require("dotenv").config();
 
 const cors = require("cors");

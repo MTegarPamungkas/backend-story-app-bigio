16e0bde7f366e68115545d079d082a04fd9ad03c
Tegar Pamungkas <mtegarpamungkas111@gmail.com>
2024-08-22 13:11:11 +0700

    update

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const storyRoutes = require("./src/frameworks/routes/storyRoutes");
+const connectDB = require("./src/config/database");
+require("dotenv").config();
+
+const cors = require("cors");
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Ganti dengan origin dari aplikasi frontendmu
+  })
+);
+app.use(express.json());
+
+app.use("/api", storyRoutes);
+app.use((req, res, next) => {
+  res.send("Run");
+});
+
+connectDB();
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
